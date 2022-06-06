89ed3c07b7e8f74435504681c71ec3df6caff306
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-06 17:49:07 +0300

    create and save user profile from user details using reciever decorators and post save signals

--- InstaApp/models.py
+++ InstaApp/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from PIL import Image
 
 # Create your models here.
 
@@ -11,7 +15,17 @@ class UserProfile(models.Model):
     name = models.CharField(max_length=60)
     following = models.ManyToManyField('UserProfile', blank=True)
     followers = models.ManyToManyField('UserProfile', blank=True)
+    joined = models.DateTimeField(auto_now_add=True, default=timezone.now)
     
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance)
+    
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.UserProfile.save
+        
 # Posts Models
 # Comments Models
 # Likes Models
