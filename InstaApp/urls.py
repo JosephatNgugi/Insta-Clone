4ebb7a47e1013d1996c461d17e28e4a396c7c86a
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-08 19:57:27 +0300

    create a view function to signup page

--- InstaApp/urls.py
+++ InstaApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
+    path('signup/', views.UserRegistration, name='signup'),
     path('', views.home, name='home'),
 ]
--- InstaApp/views.py
+++ InstaApp/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authenticate
+
+
+from .forms import SignUpForm
+
 
 # Create your views here.
 
+def UserRegistration(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            InputPassword = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=InputPassword)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'Insta/auth/registration.html', {'form': form})
+
+@login_required(login_url='/accounts/login/')
 def home(request):
     return render(request, 'Insta/home.html')
