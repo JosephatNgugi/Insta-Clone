50e2e13e26e4c86d9eabb41cf70df7909a6c096f
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-06-09 13:58:28 +0300

    create  a post form and comment form

--- InstaApp/forms.py
+++ InstaApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from.models import UserProfile
+from .models import UserPost,Comment
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Input a valid email address.')
@@ -9,3 +9,16 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class PostForm(forms.ModelForm):
+	"""Post model form"""
+
+	class Meta:
+		"""Form settings."""
+		model = UserPost
+		fields = ('caption', 'image')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['related_post', 'name' , 'created_on']
